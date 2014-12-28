baaacfde819f0350be5741dc0e489fa8cb01cfab
Ferran Arau Castell <ferran.arau.castell@gmail.com>
2014-12-28 17:11:46 +0100

    Retrieve Box from DB with hashCode generated Id

---
+++ src/data/BoxCtrlDB.java
@@ -0,0 +1,56 @@
+package data;
+
+import hibernate.HibernateUtil;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import datainterface.BoxCtrl;
+import domain.Box;
+
+public class BoxCtrlDB implements BoxCtrl {
+
+	private static BoxCtrlDB instance;		
+	public static BoxCtrlDB getInstance() {
+		if (instance == null)
+			instance = new BoxCtrlDB();
+		return instance;
+	}
+	
+	public BoxCtrlDB() {}
+
+	@Override
+	public Box get(Integer idGame, Integer numRow, Integer numCol) throws Exception {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		 
+		Box res = (Box) session.get(Box.class, Box.hashCode(idGame, numRow, numCol));
+		session.close();
+		if(res == null)
+			throw new Exception("BoxNoExist");
+		return res;
+	}
+	
+	@Override
+	public Boolean exists(Integer idGame, Integer numRow, Integer numCol) {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		 
+		Box res = (Box) session.get(Box.class, Box.hashCode(idGame, numRow, numCol));
+		session.close();
+
+		return (res != null);
+	}
+	
+	@Override
+	public List<Box> all() {
+		SessionFactory sf = HibernateUtil.getSessionFactory();
+		Session session = sf.openSession();
+		 
+		List<Box> res = (List<Box>) session.createQuery("from "+ Box.TABLE).list();
+		session.close();
+		return res;
+	}
+}
--- src/domain/Box.java
+++ src/domain/Box.java
@@ -1,5 +1,14 @@
 package domain;
 
+
 public class Box {
-	public static final String TABLE = "BOX";
+	public static final String TABLE = "BOX";	
+	
+	
+	public static int hashCode(Integer idGame, Integer numRow, Integer numCol) {
+		String id = String.format("%10d", idGame) + // Integer size
+				String.format("%5d", numRow) + 		// 99999 max rows
+				String.format("%5d", numCol);       // 99999 max cols
+		return id.hashCode();
+	}
 }
