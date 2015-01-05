0a5c985370db9a68fa58069d2b3eae81ea363390
Ferran Arau Castell <ferran.arau.castell@gmail.com>
2015-01-05 16:33:47 +0100

    MinesWeeper domain class as a singleton

--- src/domain/MinesWeeper.java
+++ src/domain/MinesWeeper.java
@@ -1,5 +1,32 @@
 package domain;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = Game.TABLE)
 public class MinesWeeper {
 	public static final String TABLE = "MINESWEEPER";
+
+	public MinesWeeper() {}
+	
+	private static MinesWeeper instance;
+	public static MinesWeeper getInstance() {
+		if (instance == null)
+			instance = new MinesWeeper();
+		return instance;
+	}
+	
+	@OneToOne
+    @PrimaryKeyJoinColumn(name = "id_game_fk", referencedColumnName = "id_game")    
+	private Game game;
+	public void setGame(Game g) {
+		game = g;
+	}
+	
+	public Game getGame() {
+		return game;
+	}	
 }
