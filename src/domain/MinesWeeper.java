f7cc81e5c14d07acdf1884dc7c2fbd00e7f22f79
Ferran Arau Castell <ferran.arau.castell@gmail.com>
2015-01-10 11:50:23 +0100

    Set minesweeper class pKey

--- src/domain/MinesWeeper.java
+++ src/domain/MinesWeeper.java
@@ -1,32 +1,37 @@
 package domain;
 
+import hibernate.HibernateUtil;
+
 import javax.persistence.Entity;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = Game.TABLE)
+@Table(name = MinesWeeper.TABLE)
 public class MinesWeeper {
 	public static final String TABLE = "MINESWEEPER";
 
-	public MinesWeeper() {}
+	@Id
+	private int id;
+	
+	public MinesWeeper() {
+		id = 0;
+		HibernateUtil.save(this);
+	}	
 	
 	private static MinesWeeper instance;
 	public static MinesWeeper getInstance() {
 		if (instance == null)
 			instance = new MinesWeeper();
 		return instance;
-	}
-	
-	@OneToOne
-    @PrimaryKeyJoinColumn(name = "id_game_fk", referencedColumnName = "id_game")    
-	private Game game;
-	public void setGame(Game g) {
-		game = g;
-	}
-	
-	public Game getGame() {
-		return game;
 	}	
+	
+	public int getId () {
+		return id;
+	}
+	
+	public void setId (int id) {
+		this.id = id;
+		HibernateUtil.update(this);
+	}
 }
