a50d57806bda059f97c5b71355f343322b1ad1b5
Ferran Arau Castell <ferran.arau.castell@gmail.com>
2015-01-10 11:54:08 +0100

    Hidden persistence inside creators

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import domain.Admin;
 import domain.Game;
 import domain.Level;
+import domain.MinesWeeper;
 import domain.Player;
 import domain.User;
 
@@ -16,36 +17,26 @@ public class Main {
 	public static void main(String[] args) {
 		Level easy      = new Level("easy", 	10, 10, 10);
 		Level medium    = new Level("medium", 	20, 20, 20);
-		Level difficult = new Level("difficult",30, 30, 30);
-		HibernateUtil.save(easy);
-		HibernateUtil.save(medium);
-		HibernateUtil.save(difficult);
+		Level difficult = new Level("difficult",30, 30, 30);		
 	
 
 		// User checks		
-		User u = new User ("u1", "n1", "n12", "pwd");
-		HibernateUtil.save(u);
+		User u = new User ("u1", "n1", "n12", "pwd");		
 		
 		Admin admin = new Admin("a1", "n2", "n22", "pwd", "933843321");
 		Player player = new Player("p1", "n3", "n32", "pwd", "p1@p1.com");
 		Player p2 = new Player("p2", "n4", "n42", "pwd", "p2@p2.com");
 		Player p3 = new Player("p3", "n4", "n42", "pwd", "p3@p3.com");
 
-		HibernateUtil.save(admin);
-		HibernateUtil.save(player);		
-		HibernateUtil.save(p2);		
-		HibernateUtil.save(p3);
 		
+		MinesWeeper mw = MinesWeeper.getInstance();
+		int id = mw.getId();
 		
-		Game g1 = new Game(player, easy);
-		Game g2 = new Game(p2, medium);
-		Game g3 = new Game(p3, difficult);
-		Game g4 = new Game(p2, easy);
+		Game g1 = new Game(1, player, easy);
+		Game g2 = new Game(2, p2, medium);
+		Game g3 = new Game(3, p3, difficult);
+		Game g4 = new Game(4, p2, easy);
 		
-		HibernateUtil.save(g1);
-		HibernateUtil.save(g2);
-		HibernateUtil.save(g3);
-		HibernateUtil.save(g4);
 		
 		p2.setOldGame(g2);
 		p2.setCurrentGame(g4);
@@ -65,6 +56,7 @@ public class Main {
 		String l3 = res3.getLevelName();
 		
 		System.out.println(l1 + " " + l2 + " " + l3);
+		System.out.println(l1);
 		
 		User 	res_u = (User) 	session.get(User.class, 	u.getUserName());
 		Admin	res_a = (Admin) session.get(Admin.class, 	admin.getUserName());
@@ -83,5 +75,4 @@ public class Main {
 		
 		session.close();				
 	}
-
 }
